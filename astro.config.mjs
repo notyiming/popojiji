e42447a6fafa6f6697c5eb29970217837e6a44f3
peepojiji <55159184+peepojiji@users.noreply.github.com>
2025-01-19 22:09:47 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://peepojiji.github.io",
-  base: "popojiji",
+  base: "",
   trailingSlash: "always",
   integrations: [
     tailwind(
